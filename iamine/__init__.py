2be16fe0876b0a9a12e49f65b77abd8d54701746
Jake Johnson <jake@archive.org>
2015-01-28 14:44:31 -0800

    Added licensing details, IA copyright, etc.

--- iamine/__init__.py
+++ iamine/__init__.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
+"""
+IA Mine
+~~~~~~~
+
+Concurrently retrieve metadata from Archive.org items.
+
+:copyright: (c) 2015 by Internet Archive.
+:license: AGPL 3, see LICENSE for more details.
+
+"""
+
+__title__ = 'iamine'
+__version__ = '0.0.1'
+__author__ = 'Jacob M. Johnson'
+__license__ = 'AGPL 3'
+__copyright__ = 'Copyright 2015 Internet Archive'
+
+
 import argparse
 import logging
 import signal
--- setup.py
+++ setup.py
@@ -4,6 +4,11 @@ from setuptools import setup
 setup(
     name = 'iamine',
     version = '0.1',
+    author='Jacob M. Johnson',
+    author_email='jake@archive.org',
+    url='https://github.com/jjjake/iamine',
+    license='AGPL 3',
+    description='Concurrently retrieve metadata from Archive.org items.',
     packages=['iamine'],
     install_requires = [
         'aiohttp==0.14.1',
@@ -14,4 +19,12 @@ setup(
             'ia-mine = iamine:main',
         ],
     }
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+    ],
 )
