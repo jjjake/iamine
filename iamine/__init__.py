af5ca7f17d75e803dfbc6115529c7c95837b9329
jake <jake@archive.org>
2015-08-03 14:23:14 -0700

    v0.3.2

--- iamine/__init__.py
+++ iamine/__init__.py
@@ -9,7 +9,7 @@ Internet Archive data mining tools.
 
 """
 __title__ = 'iamine'
-__version__ = '0.3.1'
+__version__ = '0.3.2'
 __url__ = 'https://github.com/jjjake/iamine'
 __author__ = 'Jacob M. Johnson'
 __license__ = 'AGPL 3'
