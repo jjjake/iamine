2e3e26d95cd0a1c58db796bb5afcac82a284f67e
jake <jake@archive.org>
2015-08-03 14:36:36 -0700

    Added long_description with release history.

--- setup.py
+++ setup.py
@@ -6,8 +6,13 @@ from codecs import open
 
 
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
-with open('iamine/__init__.py', 'rb', 'utf-8') as f:
+with open('iamine/__init__.py', 'r', 'utf-8') as f:
     version = str(ast.literal_eval(_version_re.search(f.read()).group(1)))
+
+with open('README.rst', 'r', 'utf-8') as f:
+    readme = f.read()
+with open('HISTORY.rst', 'r', 'utf-8') as f:
+    history = f.read()
 
 if sys.version_info <= (3,):
     sys.stderr.write('"iamine" requires a Python version greater than 3.3.')
@@ -30,6 +35,7 @@ setup(
     url='https://github.com/jjjake/iamine',
     license='AGPL 3',
     description='Concurrently retrieve metadata from Archive.org items.',
+    long_description=readme + '\n\n' + history,
     packages=['iamine'],
     install_requires = install_requires,
     entry_points = {
